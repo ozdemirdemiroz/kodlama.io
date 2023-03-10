a8b48839940c543134c3a71996ee79e43308e181
ozdemirdemiroz <118733060+ozdemirdemiroz@users.noreply.github.com>
2023-03-10 08:43:35 +0300

    Add files via upload

---
+++ kodlama.o_odev1.py
@@ -0,0 +1,34 @@
+
+
+# str - string - text tipi
+a='5'
+
+# int , float  -sayısal tipler
+b=5 
+c=5.1
+
+# dict - mapping tip , anahtar ve değerden oluşur
+notlar={'ahmet':[20 , 30 , 40] ,'aslı':[25 , 55 , 80]}
+
+# list, tuple - dizi tipleri
+Ahmetin_notları=[20,30,40]
+Aslının_notları=(25,55,80)
+
+# bool -  bolean  /true ve false
+d=False
+
+# set - küme 
+g={'elma','armut','muz'}
+
+
+temp=35.5
+if temp>24:
+    print(f"oda sıcaklığı {temp} C , ısıtıcı açılacak")
+
+elif temp<21:
+
+    print(f"oda sıcaklığı {temp} C, ısıtıcı kapanacak")
+
+else:
+
+    print(f"oda sıcaklığı {temp} C")
