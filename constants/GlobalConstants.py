ff8752ae74124f86bbf0155d12bc861490199336
ozdemirdemiroz <118733060+ozdemirdemiroz@users.noreply.github.com>
2023-04-01 18:54:38 +0300

    gün 6 ödev

---
+++ constants/GlobalConstants.py
@@ -0,0 +1,5 @@
+URL="https://www.saucedemo.com/"
+NAME="Özdemir"
+SURNAME="Demiröz"
+ZIPCODE="06530"
+COMPLETE="Your order has been dispatched, and will arrive just as fast as the pony can get there!"
